5ed3720e00fc57a50766c5f049dfd6ef8de3fcd7
Nurettin Şen <nurie487@gmail.com>
2022-07-16 19:14:54 +0300

    Update module prisma

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { PrismaClient } from '@prisma/client';
 
-@Module({})
-export class PrismaModule {}
+@Module({
+  providers: [PrismaService],
+})
+//db setting
+export class PrismaModule extends PrismaClient {
+  //
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:mypassword@localhost:5432/postgres?schema=public',
+        },
+      },
+    });
+  }
+}
