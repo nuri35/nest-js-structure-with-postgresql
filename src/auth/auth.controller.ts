0bbbbc2faf03c4b6ae1ba421eca3da140487cae9
Nurettin Şen <nurie487@gmail.com>
2022-07-16 15:37:50 +0300

    Add structure

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller()
-export class AuthControler {}
+@Controller('auth')
+export class AuthControler {
+  //   authService: AuthService; // private yaptıgım ıcın gerek yok ve constructor ıcındede yazmama gerek yok
+  constructor(private authService: AuthService) {
+    //
+    // this.authService = 2; calısmaz
+    // this.authService = authService; calısır geçerli olan parametre yoladıgımda
+    // this.authService = {
+    //   test() {
+    //     console.log('hell word');
+    //   },
+    // }; //calısır************* yada  tipi obje ıcınde fonks alıyor zaten this.authService.login(); desende calısıyor videoda gosterdi
+    /** */
+  }
+  @Post('signup')
+  signup() {
+    return 'ı am signup';
+  }
+  @Post('signin')
+  signin() {
+    return 'ı am signin';
+  }
+}
