968f6c6d245cfe79ff92ae87782ba158f6313818
Nurettin Şen <nurie487@gmail.com>
2022-08-24 21:37:33 +0300

    update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
+
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import { JwtGuard } from '../auth/guard/index';
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@GetUser() user: User) {
     //@GetUser() dedııgmız şeyde decorator aslında switchToHttp().getRequest() istek atarak req dekı userı alıyor onuda  user: User burda user'a atıyor user'ında tipini User entitiysi olarak belırledık zaten
     //User dedıgımız aslında userEntity işte db olarak tanımladıgımız dosya  parametredekı user'a da tip olarak atadık
     return user;
+  }
+
+  @Patch()
+  editUser() {
+    //
   }
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  //   constructor() {}
+  //   async editUser(userId: number) {}
+}
+
+//burda kal 3.06
