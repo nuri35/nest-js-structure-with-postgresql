e943e82cbf21f99fc4ed2e71e758b76b79c86483
Nurettin Şen <nurie487@gmail.com>
2022-08-26 20:51:15 +0300

    Add user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import { JwtGuard } from '../auth/guard/index';
+import { EditUserDto } from './dto/edit-user.dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get('me')
   getMe(@GetUser() user: User) {
     //@GetUser() dedııgmız şeyde decorator aslında switchToHttp().getRequest() istek atarak req dekı userı alıyor onuda  user: User burda user'a atıyor user'ında tipini User entitiysi olarak belırledık zaten
@@ -15,7 +18,7 @@ export class UserController {
   }
 
   @Patch()
-  editUser() {
-    //
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  //   constructor() {}
-  //   async editUser(userId: number) {}
+  constructor(private prisma: PrismaService) {}
+  async editUser(userId: number) {}
 }
-
-//burda kal 3.06
